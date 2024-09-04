7b13679c3c93d5f93e34384ae4995417c6242c2b
lucaskja <lucaskja@usp.br>
2024-09-04 20:22:47 -0300

    feat(add): initial spring

---
+++ src/main/java/br/com/mestradousp/gerenciadorformularios/GerenciadorformulariosApplication.java
@@ -0,0 +1,13 @@
+package br.com.mestradousp.gerenciadorformularios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorformulariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorformulariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mestradousp/gerenciadorformularios/GerenciadorformulariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mestradousp.gerenciadorformularios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorformulariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
