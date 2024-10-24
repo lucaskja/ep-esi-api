2eb645dc32ca76ed015b4a50b5582e0c384f98b8
lucaskja <lucaskja@usp.br>
2024-10-24 17:33:13 -0300

    feat(Security): add Cors support

--- src/main/java/br/com/mestradousp/gerenciadorformularios/config/SecurityConfig.java
+++ src/main/java/br/com/mestradousp/gerenciadorformularios/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET, "/api/ccp/email").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/performance-report/student/**").hasRole(STUDENT.name())
--- src/main/java/br/com/mestradousp/gerenciadorformularios/controller/AuthenticationController.java
+++ src/main/java/br/com/mestradousp/gerenciadorformularios/controller/AuthenticationController.java
@@ -21,11 +21,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.parameters.P;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/auth")
